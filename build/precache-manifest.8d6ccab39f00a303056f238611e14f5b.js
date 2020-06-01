eecf2d1ed32c8db74c95b605beba8b4504aac091
Charles Uthulor <charlesnnanna1667@gmail.com>
2020-06-01 08:06:45 -0700

    Deployed to Heroku

---
+++ build/precache-manifest.8d6ccab39f00a303056f238611e14f5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65b7645069709369e272321e040d5e2c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8404427d2b0f238a940e",
+    "url": "/static/css/main.c16c764b.chunk.css"
+  },
+  {
+    "revision": "fbe71a61230e1795cfd5",
+    "url": "/static/js/2.80deda56.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/static/js/2.80deda56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8404427d2b0f238a940e",
+    "url": "/static/js/main.4f985c0e.chunk.js"
+  },
+  {
+    "revision": "4d8de78eaf2788cc3b84",
+    "url": "/static/js/runtime-main.b096108f.js"
+  }
+]);
